6040fb7153cc652fc0f03c3fdeecb16bba877f94
Ceihiro <ceihiroadriatico@gmail.com>
2025-05-27 09:14:53 +0800

    initial commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'main.dart';  // Adjust this import path if splash_screen.dart is in a subfolder
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/NventoryBud.png'), // Your PNG image path
+      ),
+    );
+  }
+}
